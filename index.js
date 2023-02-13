f1957e8fface0b9b4d30ee4ca651892cfe773abc
Alexander Luna <alexfox_2000@hotmail.com>
2023-02-13 11:31:14 -0500

    connect to mongodb atlas

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
+
+//setting up mongodb
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose
+  .connect(process.env.MONGO_URI, connectionParams)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the databse " + err);
+  });
 
 app.use(cors());
 app.use(express.static(__dirname + "/public"));
